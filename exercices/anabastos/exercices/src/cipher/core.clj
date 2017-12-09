4b23ce0b5dc1832a52d0fc9ef36f474d2464508a
Ana <ana.bastos881@gmail.com>
2017-12-09 16:00:11 -0200

    Add exercices :star:

---
+++ exercices/anabastos/exercices/src/cipher/core.clj
@@ -0,0 +1,74 @@
+(ns cipher.core
+  (:require [clojure.string :as string])
+  (import [java.lang Character]))
+
+(defn to-int
+  "Transfroma char to int."
+  [letter]
+  (let [ascii-a (int \a)]
+    (- (int letter) ascii-a)))
+
+(defn to-char
+  "Transforma int to char."
+  [n]
+  (let [ascii-a (int \a)]
+    (char (+ n ascii-a))))
+
+(defn shift
+  "Modifica a letra de acordo com valor n."
+  [letter n]
+  (to-char (mod (+ (to-int letter) n) 26)))
+
+(defn caesar-encrypt
+  "Encripta frase secreta por n"
+  [text n]
+  (apply str (mapv #(shift % n) (get-letters text))))
+
+(defn caesar-decrypt
+  "Decripta frase secreta por n"
+  [secret n]
+  (apply str (mapv #(shift % (* -1 n)) secret)))
+
+(defn get-letters
+  "Filtra simbolos e coloca em underscore"
+  [word]
+  (string/lower-case (apply str (filterv #(Character/isLetter %) word))))
+
+(defn increase-frequency
+  "Função para aumentar a frequencia da palavra no hash"
+  [hash word]
+  (assoc hash word (inc (hash word 0))))
+
+(defn build-frequency-map
+  "Controi hash de frequencia de todas as palavras da frase"
+  [word]
+  (reduce increase-frequency {} word))
+
+(defn count-letter
+  "Conta letras na frase"
+  [chr word]
+  (get (build-frequency-map word) chr 0))
+
+(defn most-frequent
+  "Pega as tres letras mais frequentes em uma frase"
+  [text]
+  (take 3 (sort-by second > (build-frequency-map text))))
+
+(defn get-key
+  "Pega a diferença entre uma letra e a letra mais frequente no ingles(e, t, a)"
+  [word-most-frequent english-most-frequent]
+  (mod (- (to-int (char word-most-frequent)) (to-int (char english-most-frequent))) 26))
+
+(defn decrypt-by-key
+  "Decripta a partir das letras mais frequentes da frase"
+  [word-most-frequent test-english-key secret]
+  (let [key (get-key word-most-frequent test-english-key)]
+    (println "Key: " key " | Palavra:" word-most-frequent "| No ingles: " test-english-key)
+    (println (caesar-decrypt secret key))
+    (caesar-decrypt secret key)))
+
+(defn test-keys
+  "test keys"
+  [secret english-most-frequent]
+  (let [frequencies (most-frequent secret)]
+    (map #(decrypt-by-key (first %) english-most-frequent secret) frequencies)))
---
+++ exercices/anabastos/exercices/test/cipher/core_test.clj
@@ -0,0 +1,83 @@
+(ns cipher.core-test
+  (:require [cipher.core :as core]
+          [midje.sweet :refer :all]))
+
+(facts "takes a lowercase letter character and returns its position in the alphabet: a = 0, b = 1, etc"
+  (tabular
+    (core/to-int ?char) => ?result
+    ?char ?result
+    \a    0
+    \b    1
+    \c    2
+    \d    3))
+
+(facts "takes a num and returns lowercase letter"
+  (tabular
+    (core/to-char ?n) => ?result
+    ?n ?result
+    0    \a
+    1    \b
+    2    \c
+    3   \d))
+
+(facts "shifts letter :)"
+  (tabular
+    (core/shift ?n 3) => ?result
+    ?n ?result
+    \a    \d
+    \b    \e
+    \c    \f
+    \d   \g))
+
+
+(fact "shifts alfa"
+  (core/caesar-encrypt "abcdefghijklmnopqrstuvwxyz" 3) => "defghijklmnopqrstuvwxyzabc")
+
+(fact "shifts alfa"
+  (core/caesar-encrypt "Hello, friend!" 5) => "mjqqtkwnjsi")
+
+(fact "shifts alfa"
+  (core/caesar-decrypt "defghijklmnopqrstuvwxyzabc" 3) => "abcdefghijklmnopqrstuvwxyz")
+
+(fact "shifts alfa"
+  (core/get-letters "Hello, friend!") => "hellofriend")
+
+(facts 
+  (fact "shifts alfa"
+    (core/count-letter \a "aadvark") => 3)
+
+  (fact "shifts alfa"
+    (core/count-letter \x "aadvark") => 0)
+)
+
+(fact "get frequency"
+    (core/most-frequent "ahixblmaxmabgzpbmayxtmaxklmatmixkvaxlbgmaxlhnetgwlbgzlmaxmngxpbmahnmmaxphkwltgwgxoxklmhiltmtee") => '([\m 14] [\x 12] [\a 11]))
+
+(facts "get key :)"
+  (tabular
+    (core/get-key ?n \e) => ?result
+    ?n ?result
+    \m   8
+    \c   24))
+
+
+(fact "decrypttest"
+  (core/decrypt-by-key \j \e "mjqqtkwnjsi") => "hellofriend")
+
+(facts "test keys"
+  (tabular
+    (core/test-keys "ahixblmaxmabgzpbmayxtmaxklmatmixkvaxlbgmaxlhnetgwlbgzlmaxmngxpbmahnmmaxphkwltgwgxoxklmhiltmtee" ?n) => ?result
+    ?n ?result
+    \e   '("szaptdespestyrhtesqplespcdesleapcnspdtyespdzfwlyodtyrdespefyphteszfeesphzcodlyoypgpcdezadlelww"
+ "hopeisthethingwithfeathersthatperchesinthesoulandsingsthetunewithoutthewordsandneverstopsatall"
+ "elmbfpqebqefkdtfqecbxqebopqexqmbozebpfkqebplrixkapfkdpqebqrkbtfqelrqqebtloapxkakbsbopqlmpxqxii")
+    \t   '("hopeisthethingwithfeathersthatperchesinthesoulandsingsthetunewithoutthewordsandneverstopsatall"
+ "wdetxhiwtiwxcvlxiwutpiwtghiwpietgrwthxciwthdjapcshxcvhiwtijctlxiwdjiiwtldgshpcsctktghidehpipaa"
+ "tabqueftqftuzsiuftrqmftqdeftmfbqdotqeuzftqeagxmzpeuzseftqfgzqiuftagfftqiadpemzpzqhqdefabemfmxx")
+    \a   '("ovwlpzaolaopundpaomlhaolyzaohawlyjolzpuaolzvbshukzpunzaolabuldpaovbaaoldvykzhukulclyzavwzhahss"
+ "dklaeopdapdejcsepdbawpdanopdwplanydaoejpdaokqhwjzoejcopdapqjasepdkqppdasknzowjzjaranopklowpwhh"
+ "ahixblmaxmabgzpbmayxtmaxklmatmixkvaxlbgmaxlhnetgwlbgzlmaxmngxpbmahnmmaxphkwltgwgxoxklmhiltmtee")))
+
+(facts "test"
+  (fact "decrypts"
+    (core/caesar-decrypt "ahixblmaxmabgzpbmayxtmaxklmatmixkvaxlbgmaxlhnetgwlbgzlmaxmngxpbmahnmmaxphkwltgwgxoxklmhiltmtee" 19) => "hopeisthethingwithfeathersthatperchesinthesoulandsingsthetunewithoutthewordsandneverstopsatall"))
